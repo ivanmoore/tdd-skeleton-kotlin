9f874c2fa701c5acd6677a93be463d81fd6a5fc6
Ivan Moore <ivan@teamoptimization.com>
2025-03-16 13:52:21 +0000

    convert to kotlin gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.0.20"
+    `kotlin-dsl`
+    jacoco
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("com.natpryce:hamkrest:1.8.0.1")
+}
